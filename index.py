cdcfaa46ed34eb61b7ac7eaed3d14f4ec3df15bc
Filipe de Bianchi Andrade <fbianchi.andrade@gmail.com>
2023-10-01 17:40:15 -0300

    criando classe categorias

--- index.py
+++ index.py
@@ -1,5 +1,9 @@
 from classes.Produto import Produto
+from classes.Categoria import Categoria
 
 
-produto = Produto('002', 'Notebook', 50, 4000)
-produto.inserir()
+# produto = Produto('002', 'Notebook', 50, 4000)
+# produto.inserir()
+
+categoria = Categoria('Roupas')
+categoria.inserir()
