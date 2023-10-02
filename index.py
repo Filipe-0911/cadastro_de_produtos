d39c79814f89bf99b59b9a05494b6c813fd86fac
Filipe <fbianchi.andrade@gmail.com>
2023-10-02 17:40:27 -0300

    adicionando filtro para verificar se o produto já está cadastrado

--- index.py
+++ index.py
@@ -1,17 +1,33 @@
 from classes.Produto import Produto
 from classes.Categoria import Categoria
 
-Produto.excluir(0)
-# item = 4
-# itemAlterar = Produto.lerArquivo(item)
+def menu():
+    print()
+    print("1 - Listar Produtos")
+    print("2 - Inserir Produto")
+    print("3 - Alterar Produto")
+    print("4 - Excluir Produto")
+    print("0 - Sair")
+    print()
 
-# produto = Produto(itemAlterar['codigo'], itemAlterar['nome'], 200, 150)
-# produto.alterar(item)
+opcao = 1
+while opcao !=0:
+    menu()
+    opcao = int(input('Escolha uma opção: '))
 
-# produto = Produto('005', 'Tenis', 170, 599)
-# produto.inserir()
-# produto.listarTodos()
+    match opcao:
+        case 1: 
+            print('********************************************************************************')
+            Produto.listarTodos()
+            print('********************************************************************************')
 
-# categoria = Categoria('calçados')
-# categoria.inserir()
-# categoria.listarTodos()
+        case 2: 
+            codigo = input('Informe o código do PRODUTO: ')
+            nome = input('Informe o nome do PRODUTO: ')
+            quantidade = input('Informe o quantidade do PRODUTO: ')
+            valor = input('Informe o valor do PRODUTO: ')
+
+            produto = Produto(codigo, nome, quantidade, valor)
+            produto.inserir()
+        case 3: pass
+        case 4: pass
